d96ae17622ff545f53a7937578ca8cf5664ccf49
Alex <dnjscksdn98@gmail.com>
2020-04-13 22:02:08 +0900

    Create: Board 컨트롤러 클래스 생성

---
+++ src/main/java/com/spring_tutorial/board/controller/BoardController.java
@@ -0,0 +1,21 @@
+package com.spring_tutorial.board.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/board/*")
+public class BoardController {
+
+}
