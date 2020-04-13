bcda897ea8b981cc08328bc283e6f75279e544b6
Alex <dnjscksdn98@gmail.com>
2020-04-13 22:20:13 +0900

    Create: Board Service 인터페이스 생성
    전체 게시글 목록, 글 작성, 상세보기, 수정, 삭제, 조회수 증가 기능을 포함한다

---
+++ src/main/java/com/spring_tutorial/board/service/BoardService.java
@@ -0,0 +1,23 @@
+package com.spring_tutorial.board.service;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import com.spring_tutorial.board.model.dto.BoardDto;
+import com.spring_tutorial.board.model.dao.BoardDao;
+
+public interface BoardService {
+	
+	public List<BoardDto> listAll() throws Exception;
+	
+	public void create(BoardDto dto) throws Exception;
+	
+	public BoardDto detail(int boardId) throws Exception;
+	
+	public void update(BoardDto dto) throws Exception;
+	
+	public void delete(int boardId) throws Exception;
+	
+	public void increaseViews(int boardId, HttpSession session) throws Exception;
+}
